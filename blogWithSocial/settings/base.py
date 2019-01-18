a0ecda8d6a85f3e1574887136fdff15546ea3fa9
spacelinkers <josen221@gmail.com>
2019-01-18 19:04:23 +0600

    migrate

--- blogWithSocial/settings/base.py
+++ blogWithSocial/settings/base.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'qokn)%8)087w6+$h943#leih!dtu1!vojoe9j6^+9^xwt0u#c#') 
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
